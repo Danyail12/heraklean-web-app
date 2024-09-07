2109097cdde7cbd50a5869b488f4a8453f559ed8
kumail-bradhub <141053200+Danyail12@users.noreply.github.com>
2024-09-07 14:30:19 +0500

    REVERSE

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['module-to-externalize'],
-      build: {
-        outDir: 'dist',
-      },
-    }
-  }
 })
-
