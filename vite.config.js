87fc3677e5384baea042ff534bfc656488508f4d
kumail-bradhub <141053200+Danyail12@users.noreply.github.com>
2024-09-07 14:43:22 +0500

    check

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -10,7 +9,4 @@ export default defineConfig({
       external: ['bootstrap'] // Externalize Bootstrap (optional)
     }
   }
-
 })
-
-
